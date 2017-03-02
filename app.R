987f572ae7bf1abe20d3b7b8d187eddd29b8c5f2
Ron Wu <rcw278@nyu.edu>
2017-03-02 14:22:14 -0500

    final

--- app.R
+++ app.R
@@ -1,11 +1,27 @@
 library(shiny)
 library(shinythemes)
- 
+
+#########################
+# read hdf5 and save it to rds
+#########################
+
+# library(rhdf5)
+# library(data.table)
+# setwd("/Users/ronnnwu/Desktop/Kaggle/2_sigma/shiny/")
+# h5ls("../input/train.h5")
+# data <- h5read("../input/train.h5","train")
+# names <- data[['axis0']]
+# data <- cbind(t(data[['block0_values']]),t(data[['block1_values']]))
+# data <- as.data.frame(data)
+# colnames(data) <- names
+# saveRDS(data, "df.rds")
+
 
 df <- readRDS("df.rds")    
+ 
 
 stockIDs <- sort(unique(df$id))
-indicators <- colnames(df)[4:111]
+indicators <- colnames(df)[3:110]
 
 ui <- navbarPage("2 Sigma ML Contest",
   
@@ -45,7 +61,7 @@ server <- function(input, output){
     
     
     par(mar=c(5, 4, 4, 6) + 0.1)
-    plot(dID$timestamp, dID$y, pch=16,  axes=FALSE,  xlab="", ylab="", 
+    plot(dID$timestamp, dID$y, pch=16,  axes=FALSE,  xlab="", ylab="", xlim=c(1, 1812),
          type="l",col="black", main =paste0("stockID: ",input$StockID, ", Indicator: ", input$indicator) )
     axis(2 ,col="black",las=1)   
     mtext("",side=2,line=2.5)
@@ -53,7 +69,7 @@ server <- function(input, output){
     
     if (!all(is.na( dID[,input$indicator] ))){ 
       par(new=TRUE) 
-      plot(dID$timestamp, dID[,input$indicator], pch=15,  xlab="", ylab="",
+      plot(dID$timestamp, dID[,input$indicator], pch=15,  xlab="", ylab="", xlim=c(1, 1812),
            axes=FALSE, type="l", col="blue") 
       mtext("",side=4,col="blue") 
       axis(4, col="blue",col.axis="blue",las=1)  
